05e82806553ff57364eb144c5a9497e9b5f34c64
bhanubhatt91 <103954506+bhanubhatt91@users.noreply.github.com>
2022-05-31 22:18:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Legacy cars</h1>
+      <h3 data-ns-test="project-description"> Its an online website for auction of vintage cars and allows user to live bid </h3>
+    </div>
   )
 }
 
